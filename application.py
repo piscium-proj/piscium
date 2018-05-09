b2446d079400bff377fcdf588664dcabf588bbca
mwu <menglongwu3@gmail.com>
2018-05-09 21:37:06 +0200

    code review

--- application.py
+++ application.py
@@ -1,28 +1,14 @@
 #-*-coding: utf-*-
 
-import os
 import tornado.web
-from tornado.web import url
 from tornado.options import define, options
-
-from handles import IndexHandler
+from handlers.urls import handlers
+from config import settings
 
 define ('port', default=8000, help='run on the given port', type=int)
 
 class Application(tornado.web.Application):
     def __init__(self):
-        handlers = [
-            (r'/', IndexHandler),
-            (r'/.*', tornado.web.RedirectHandler,{'url':'/'})
-        ]
-
-        settings = dict(
-            template_path = os.path.join(os.path.dirname(__file__), 'templates'),
-            static_path = os.path.join(os.path.dirname(__file__), 'static'),
-            web_title = '',
-            cookie_secrets = "b'B54pC+IfQvi/URHzbEUM+I4qazxvHEXZgXiRN2lWtBk='",
-            xsrf_cookies = True,
-        )
-        tornado.web.Application.__init__(self,handlers, **settings)
+        tornado.web.Application.__init__(self, handlers, **settings)
 
 application = Application()
--- handlers/__init__.py
+++ handlers/__init__.py
@@ -3,12 +3,29 @@
 from tornado.web import RequestHandler
 
 class BaseHandler(RequestHandler):
+    """Basic handlers classes"""
+
     def get_current_user(self):
         user_id = self.get_secure_cookie('uid')
         if not user_id:
             return None
         return json_decode(user_id)
 
-class IndexHandler(RequestHandler):
+    def initialize(self):
+        pass
+
+    def on_finish(self):
+        pass
+
+    def prepare(self):
+        pass
+
+    def set_default_headers(self):
+        pass
+
+    def write_error(self, status_code, **kwargs):
+        pass
+
+class IndexHandler(BaseHandler):
     def get(self, *args, **kwargs):
         self.render('login.html', error="")
